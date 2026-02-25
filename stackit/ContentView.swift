848bd8d09b4ff4173ef563b5037e81628f64bfb0
sakshmenon <saksh585@gmail.com>
2026-02-25 10:51:49 -0500

    Initial Commit

---
+++ stackit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stackit
+//
+//  Created by saksh menon on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stackit/stackitApp.swift
@@ -0,0 +1,17 @@
+//
+//  stackitApp.swift
+//  stackit
+//
+//  Created by saksh menon on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct stackitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
